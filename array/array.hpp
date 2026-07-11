acb804df38f9de05b3c5aaeb4163fce4247f6b78
Alieksandr Bilyayev <bilyayeva.dev@gmail.com>
2026-07-11 21:21:56 +0200

    feat(array): add explicit specialization for empty array (N = 0)
    - Implemented sfs::array<T, 0> specialization with safe data() returning nullptr.
    
    - Updated std::tuple_size and std::tuple_element to support N = 0 without errors.
    
    - Added std::get overloads in std namespace for STL integration and structured bindings.

--- array/array.hpp
+++ array/array.hpp
@@ -193,6 +193,170 @@ namespace sfs {
     };
 
 //
+//  Explicit specialization for empty array (N = 0)
+//
+
+    template<class T>
+    struct array<T, 0> {
+    
+    //
+    // Aliases
+    //
+        
+        using value_type                = T;
+        using size_type                 = std::size_t;
+        using difference_type           = std::ptrdiff_t;
+        using reference                 = value_type&;
+        using const_reference           = const value_type&;
+        using pointer                   = value_type*;
+        using const_pointer             = const value_type*;
+        using iterator                  = value_type*;
+        using const_iterator            = const value_type*;
+        using reverse_iterator          = std::reverse_iterator<iterator>;
+        using const_reverse_iterator    = std::reverse_iterator<const_iterator>;
+
+    //
+    // There is no underlying storage, because array is empty
+    //
+
+    //
+    // Element Access (N = 0)
+    //
+
+        constexpr reference at( size_type pos ) {
+            throw std::out_of_range( "sfs::array::at: position >= size (N == 0)" );
+        }
+
+        constexpr const_reference at( size_type pos ) const {
+            throw std::out_of_range( "sfs::array::at: position >= size (N == 0)" );
+        }
+
+        constexpr reference operator[]( size_type pos ) noexcept {
+            return *data();
+        }
+
+        constexpr const_reference operator[]( size_type pos ) const noexcept {
+            return *data();
+        }
+
+        constexpr reference front() noexcept {
+            return *data();
+        }
+
+        constexpr const_reference front() const noexcept {
+            return *data();
+        }
+
+        constexpr reference back() noexcept {
+            return *data();
+        }
+
+        constexpr const_reference back() const noexcept {
+            return *data();
+        }
+
+        constexpr pointer data() noexcept {
+            return nullptr;
+        }
+
+        constexpr const_pointer data() const noexcept {
+            return nullptr;
+        }
+        
+    //
+    // Iterators (N = 0)
+    //
+
+        constexpr iterator begin() noexcept {
+            return nullptr;
+        }
+
+        constexpr const_iterator begin() const noexcept {
+            return nullptr;
+        }
+
+        constexpr const_iterator cbegin() const noexcept {
+            return this->begin();
+        }
+
+        constexpr iterator end() noexcept {
+            return nullptr;
+        }
+
+        constexpr const_iterator end() const noexcept {
+            return nullptr;
+        }
+
+        constexpr const_iterator cend() const noexcept {
+            return this->end();
+        }
+
+        constexpr reverse_iterator rbegin() noexcept {
+            return reverse_iterator( this->end() );
+        }
+
+        constexpr const_reverse_iterator rbegin() const noexcept {
+            return const_reverse_iterator( this->end() );
+        }
+
+        constexpr const_reverse_iterator crbegin() const noexcept {
+            return this->rbegin();
+        }
+
+        constexpr reverse_iterator rend() noexcept {
+            return reverse_iterator( this->begin() );
+        }
+
+        constexpr const_reverse_iterator rend() const noexcept {
+            return const_reverse_iterator( this->begin() );
+        }
+
+        constexpr const_reverse_iterator crend() const noexcept {
+            return this->rend();
+        }
+
+    //
+    // Capacity (N = 0)
+    //
+
+        constexpr bool empty() const noexcept {
+            return true;
+        }
+
+        constexpr size_type size() const noexcept {
+            return 0;
+        }
+
+        constexpr size_type max_size() const noexcept {
+            return this->size();
+        }
+
+    //
+    // Operations (N = 0)
+    //
+
+        constexpr void fill ( const_reference value ) noexcept {}
+
+        constexpr void swap( array& other ) noexcept( std::is_nothrow_swappable_v<T> ) {}
+
+    //
+    // Non-member functions (N = 0)
+    //
+
+        friend constexpr bool operator==(const sfs::array& lhs, const sfs::array& rhs) {
+            return true;
+        }
+
+        friend constexpr auto operator<=>( const sfs::array& lhs, const sfs::array& rhs) {
+            return std::strong_ordering::equal;
+        }
+
+    };
+
+    template< class T, class... U >
+    array( T, U... ) -> array<T, 1 + sizeof...(U)>;
+
+//
 // Non-member functions (continued)
 //
 
@@ -248,9 +412,32 @@ namespace std {
 
     template<std::size_t I, class T, std::size_t N>
     struct tuple_element<I, sfs::array<T,N>> {
-        static_assert( I < N, "std::tuple_element: index out of bounds");
         using type = T;
     };
+
+//
+// Explicit specialization for empty array (N = 0)
+//
+
+    template<std::size_t I, class T, std::size_t N>
+    constexpr T& get( sfs::array<T, N>& a ) noexcept {
+        return sfs::get<I>(a);
+    }
+
+    template<std::size_t I, class T, std::size_t N>
+    constexpr T&& get( sfs::array<T, N>&& a ) noexcept {
+        return sfs::get<I>(std::move(a));
+    }
+
+    template<std::size_t I, class T, std::size_t N>
+    constexpr const T& get( const sfs::array<T, N>& a ) noexcept {
+        return sfs::get<I>(a);
+    }
+
+    template<std::size_t I, class T, std::size_t N>
+    constexpr const T&& get( const sfs::array<T, N>&& a ) noexcept {
+        return sfs::get<I>(std::move(a));
+    }
 
 }
 
