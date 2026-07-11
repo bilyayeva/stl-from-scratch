5f4b302f437069912c194cd2e609c20273892bad
Alieksandr Bilyayev <bilyayeva.dev@gmail.com>
2026-07-11 15:24:46 +0200

    feat(array): add array.hpp with initial structure for sfs::array implementation

---
+++ array/array.hpp
@@ -0,0 +1,13 @@
+#ifndef SFS_ARRAY_HPP
+#define SFS_ARRAY_HPP
+
+#include <cstddef>
+#include <iterator>
+
+namespace sfs {
+    //
+    // Implementation of the sfs::array class
+    //
+}
+
+#endif // SFS_ARRAY_HPP
