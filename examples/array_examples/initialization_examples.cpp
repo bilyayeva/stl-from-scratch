f55c1711bd3407ac67b6d1024caf10e98b82adc8
Alieksandr Bilyayev <bilyayeva.dev@gmail.com>
2026-07-20 13:28:13 +0200

    examples(array): update initialization example

--- examples/array_examples/initialization_examples.cpp
+++ examples/array_examples/initialization_examples.cpp
@@ -3,56 +3,71 @@
 #include <sfs/array.hpp>
 
 int main() {
-    // 1. Initialize with an explicit element type and size
-    sfs::array<int, 5> a1 = {1, 2, 3, 4, 5};
+    // 1. Initialize using direct-list initialization
+    sfs::array<int, 5> a1{1, 2, 3, 4, 5};
 
     for (std::size_t i{0}; i < a1.size(); ++i) {
         std::cout << a1.at(i) << ' ';
     }
     std::cout << '\n';
+    // Output: 1 2 3 4 5
 
-    // 2. Initialize using double braces
-    sfs::array<int, 5> a2{{1, 2, 3, 4, 5}};
+    // 2. Initialize using copy-list initialization
+    sfs::array<int, 5> a2 = {1, 2, 3, 4, 5};
 
     for (std::size_t i{0}; i < a2.size(); ++i) {
         std::cout << a2.at(i) << ' ';
     }
     std::cout << '\n';
+    // Output: 1 2 3 4 5
 
-    // 3. Deduce the element type and size
-    sfs::array a3 = {1, 2, 3, 4, 5};
+    // 3. Initialize using double braces
+    sfs::array<int, 5> a3{{1, 2, 3, 4, 5}};
 
     for (std::size_t i{0}; i < a3.size(); ++i) {
         std::cout << a3.at(i) << ' ';
     }
     std::cout << '\n';
+    // Output: 1 2 3 4 5
 
-    // 4. Copy an existing array
-    sfs::array a4 = a3;
+    // 4. Deduce the element type and size
+    sfs::array a4 = {1, 2, 3, 4, 5};
 
     for (std::size_t i{0}; i < a4.size(); ++i) {
         std::cout << a4.at(i) << ' ';
     }
     std::cout << '\n';
+    // Output: 1 2 3 4 5
 
-    // 5. Value-initialize every element
-    sfs::array<int, 5> a5{};
+    // 5. Copy an existing array
+    sfs::array a5 = a4;
 
-    for (const int element : a5) {
-        std::cout << element << ' ';
+    for (std::size_t i{0}; i < a5.size(); ++i) {
+        std::cout << a5.at(i) << ' ';
     }
     std::cout << '\n';
+    // Output: 1 2 3 4 5
 
-    // 6. Initialize the first elements and value-initialize the rest
-    sfs::array<int, 5> a6 = {1, 2};
+    // 6. Value-initialize every element
+    sfs::array<int, 5> a6{};
 
-    for (const int element : a6) {
-        std::cout << element << ' ';
+    for (std::size_t i{0}; i < a6.size(); ++i) {
+        std::cout << a6.at(i) << ' ';
     }
     std::cout << '\n';
+    // Output: 0 0 0 0 0
 
-    // 7. Initialize an empty array
-    [[maybe_unused]] sfs::array<int, 0> a7{};
+    // 7. Initialize the first elements and value-initialize the rest
+    sfs::array<int, 5> a7 = {1, 2};
+
+    for (std::size_t i{0}; i < a7.size(); ++i) {
+        std::cout << a7.at(i) << ' ';
+    }
+    std::cout << '\n';
+    // Output: 1 2 0 0 0
+
+    // 8. Initialize an empty array
+    [[maybe_unused]] sfs::array<int, 0> a8{};
 
     return 0;
 }
